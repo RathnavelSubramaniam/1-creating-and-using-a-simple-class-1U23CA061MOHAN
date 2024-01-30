0b0ad2282c345167de8fb49a69a2cd19e7b8bf31
1U23CA061MOHAN <139437334+1U23CA061MOHAN@users.noreply.github.com>
2024-01-30 04:53:30 +0000

    submit

--- Person.java
+++ Person.java
@@ -15,8 +15,8 @@ public class Person{
     public static void main(String[] args){
         Person1 ob1 = new Person1("Jhon","Doe",30);
         Person1 ob2 = new Person1("Alice","Smith",25);
-        System.out.println("Person1 :"+ob1.GetFullName());
-        System.out.println("Person2 :"+ob2.GetFullName());
+        System.out.println("Person 1:"+ob1.GetFullName());
+        System.out.println("Person 2:"+ob2.GetFullName());
 
         double sum = ob1.Age + ob2.Age;
         double average = sum/2;
